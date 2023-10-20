a4a8ccf12c6496e8f30d11924c7e45951da94aec
ArstarGithub02 <jbeltrame@frro.utn.edu.ar>
2023-10-20 11:46:36 -0300

    New few lines

---
+++ 2.1.01 If Condition (Exercise 1)/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Practica 1 - If Conditional - Exercise 01
+----------------------------------------------------------------------
+English:
+Given two distinct numbers,
+show them sorted in increasing order.
+
+Spanish: 
+Dados dos números distintos, 
+mostrarlos ordenados en forma creciente.
+----------------------------------------------------------------------
+*/
+using System.ComponentModel;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Ingresar un numero");
+        int numberA = int.Parse(Console.ReadLine());
+        int numberB = int.Parse(Console.ReadLine());
+    }
+}
