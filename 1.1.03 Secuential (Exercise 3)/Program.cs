d7a55c85b8d76b3c42246cf4be10a8abf6022bd5
ArstarGithub02 <github02@arstar.com.ar>
2023-10-20 11:36:43 -0300

    First Commit

---
+++ 1.1.03 Secuential (Exercise 3)/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _1._1._03_Secuential__Exercise_3_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
