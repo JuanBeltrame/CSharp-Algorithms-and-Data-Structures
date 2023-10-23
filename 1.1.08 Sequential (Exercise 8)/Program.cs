334d4fe8fbbc526fdbf19196f37f9b6f85b16d36
ArstarGithub02 <jbeltrame@frro.utn.edu.ar>
2023-10-23 11:39:22 -0300

    new exercise update

---
+++ 1.1.08 Sequential (Exercise 8)/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Practica 1 - Sequential - Exercise 08
+----------------------------------------------------------------------
+English:
+Given a number, show the result of that number raised to the eighth power.
+
+Spanish: 
+Dado un número, mostrar el resultado de dicho número elevado a la octava.
+----------------------------------------------------------------------
+*/
+
+using System.Security.Cryptography.X509Certificates;
+
+int number, raised;
+
+Console.WriteLine("Ingrese un numero");
+number = int.Parse(Console.ReadLine());
+
+raised = number * number * number * number * number * number * number * number;
+Console.WriteLine("El resultado obtenido es: "+raised);
