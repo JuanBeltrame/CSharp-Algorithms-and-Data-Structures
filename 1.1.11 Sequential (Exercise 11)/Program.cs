aa278823c1dca8fc819d20f495f99db7b43f9874
ArstarGithub02 <jbeltrame@frro.utn.edu.ar>
2023-10-23 12:11:40 -0300

    New exercise

---
+++ 1.1.11 Sequential (Exercise 11)/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Practica 1 - Sequential - Exercise 11
+----------------------------------------------------------------------
+English:
+Calculate the salary of a worker, 
+knowing the number of hours worked in the month and the hourly wage.
+
+Spanish: 
+Calcular el sueldo de un operario 
+conociendo la cantidad de horas que trabajó en el mes y el jornal horario.
+----------------------------------------------------------------------
+*/
+
+double precioHora, sueldo;
+int horasTrabajadas;
+
+precioHora = 750;
+Console.WriteLine("Ingresar las horas trabajadas del operario");
+horasTrabajadas = int.Parse(Console.ReadLine());
+
+sueldo = precioHora * horasTrabajadas;
+Console.WriteLine("El sueldo del operario es de: "+sueldo);
