5c41a3ca3fd85d82b4110f05965898b89385267e
ArstarGithub02 <jbeltrame@frro.utn.edu.ar>
2023-10-23 12:45:06 -0300

    new exercise

---
+++ 1.1.07 Sequential (Exercise 7)/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Practica 1 - Sequential - Exercise 07
+----------------------------------------------------------------------
+English:
+Given the value of the side of a square, 
+calculate its perimeter and its area, 
+and report them with explanatory signs.
+
+Spanish: 
+Dado el valor del lado de un cuadrado calcular su perímetro y su superficie, e informar
+los mismos con carteles aclaratorios.
+----------------------------------------------------------------------
+*/
+int lado;
+double perimetro, area;
+Console.Write("Ingrese el lado de un cuadrado: ");
+lado = int.Parse(Console.ReadLine());
+
+perimetro = 4 * lado;
+Console.WriteLine("El perimetro del cuadrado es: " + perimetro);
+area = lado * lado;
+Console.WriteLine("El area del cuadrado es:" + area);
