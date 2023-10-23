14c7b1ca8293bb503813496a9237241d03e8511c
ArstarGithub02 <jbeltrame@frro.utn.edu.ar>
2023-10-23 10:54:14 -0300

    New Updates on files

--- 1.1.02 Sequential (Exercise 2)/Program.cs
+++ 1.1.02 Sequential (Exercise 2)/Program.cs
@@ -1,23 +1,23 @@
 ﻿/*
-Practica 1 - Sequential - Exercise 01
+Practica 1 - Sequential - Exercise 02
 ----------------------------------------------------------------------
 English:
 Given two numbers entered via keyboard as data, 
-calculate their sum and report the result. 
+calculate their product and report the result. 
 Declare the variables to be used: N1, N2, SUM, all of numerical type.
 
 Spanish: 
 Dados como datos (ingresados por teclado) dos números cualesquiera 
-calcular su suma e informar el resultado.
+calcular su producto e informar el resultado.
 Declarar las variables a utilizar N1, N2, SUMA de tipo numérico
 ----------------------------------------------------------------------
 */
 
-int n1, n2, sum;
+int n1, n2, producto;
 
-Console.WriteLine("Ingresar 2 numeros");
+Console.WriteLine("Ingresar 2 Numeros");
 n1 = int.Parse(Console.ReadLine());
 n2 = int.Parse(Console.ReadLine());
+producto = n1 * n2;
+Console.WriteLine("El resultado de la multiplicacion es:"+ producto);
 
-sum = n1 + n2;
-Console.WriteLine("El resultado es:{0}", sum);
