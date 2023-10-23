2c6cccc2f61eeab7a5f6722aaba50bf570eebd97
ArstarGithub02 <jbeltrame@frro.utn.edu.ar>
2023-10-23 11:21:15 -0300

    New files

---
+++ 1.1.05 Sequential (Exercise 5)/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Practica 1 - Sequential - Exercise 05
+----------------------------------------------------------------------
+English:
+Given a number, show the result of multiplying that number by 6, assuming you can only use the addition operator (+).
+
+Spanish: 
+Dado un número mostrar el producto de ese número por 6, suponiendo que se cuenta
+solamente con el operador suma (+).
+----------------------------------------------------------------------
+*/
+
+int numero, product;
+
+Console.WriteLine("Ingrese un numero");
+numero = int.Parse(Console.ReadLine());
+product = numero + numero + numero + numero + numero + numero;
+
+Console.WriteLine($"El resultao es {product}");
