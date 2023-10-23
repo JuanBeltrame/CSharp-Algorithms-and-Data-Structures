c9032a606efa05d32e51de40b1a96b8f07ecb9a6
ArstarGithub02 <jbeltrame@frro.utn.edu.ar>
2023-10-23 10:12:22 -0300

    New excercise

--- 1.1.03 Secuential (Exercise 3)/Program.cs
+++ 1.1.03 Secuential (Exercise 3)/Program.cs
@@ -1,10 +1,40 @@
-﻿namespace _1._1._03_Secuential__Exercise_3_
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿/*
+Practica 1 - Sequential - Exercise 03
+----------------------------------------------------------------------
+English:
+Given five numbers, 
+calculate their average 
+and report the result
+
+Spanish: 
+Dados como datos cinco números 
+obtener el promedio de los mismos 
+e informar el resultado.
+----------------------------------------------------------------------
+*/
+
+int n1,n2,n3,n4,n5,cantidadNumeros,acumulador;
+float average;
+
+cantidadNumeros = 0;
+acumulador = 0;
+Console.WriteLine("Ingresar numeros");
+
+n1 = int.Parse(Console.ReadLine());
+cantidadNumeros +=1;
+acumulador += n1;
+n2 = int.Parse(Console.ReadLine());
+cantidadNumeros +=1;
+acumulador += n2;
+n3 = int.Parse(Console.ReadLine());
+cantidadNumeros +=1;
+acumulador += n3;
+n4 = int.Parse(Console.ReadLine());
+cantidadNumeros +=1;
+acumulador += n4;
+n5 = int.Parse(Console.ReadLine());
+cantidadNumeros +=1;
+acumulador += n5;
+
+average = acumulador/cantidadNumeros;
+Console.WriteLine("El promedio de los mismos es:{0}",average);
