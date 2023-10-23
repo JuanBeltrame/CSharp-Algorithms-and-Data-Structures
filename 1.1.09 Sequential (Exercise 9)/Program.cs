465186d59e1eb945bfe92d485fd45917362b250c
ArstarGithub02 <jbeltrame@frro.utn.edu.ar>
2023-10-23 13:14:47 -0300

    new exercise

---
+++ 1.1.09 Sequential (Exercise 9)/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Practica 1 - Sequential - Exercise 09
+----------------------------------------------------------------------
+English:
+Keep in mind that only the 4 basic operations are recognized.
+What would be the algorithm if the machine also has the power operation?
+
+Spanish: 
+Tener en cuenta, que solo se reconocen 4 operaciones básicas. ¿Cuál sería el
+algoritmo si la máquina cuenta además con la operación potencia?
+----------------------------------------------------------------------
+*/
+
+int numero, potencia;
+double powerOperation;
+
+Console.WriteLine("Ingrese un numero:");
+numero = int.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese a que potencia desea elevar el valor del numero Ingresado");
+potencia = int.Parse(Console.ReadLine());
+powerOperation = Math.Pow(numero,potencia);
+Console.WriteLine("El Resultado es: "+ powerOperation);
+
+
