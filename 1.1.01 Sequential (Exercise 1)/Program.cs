808ee5e465e4b176613e8e7355473082a18b82d9
ArstarGithub02 <jbeltrame@frro.utn.edu.ar>
2023-10-23 10:38:08 -0300

    New exercise upadate

--- 1.1.01 Sequential (Exercise 1)/Program.cs
+++ 1.1.01 Sequential (Exercise 1)/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Practica 1 - Sequential - Exercise 01
+----------------------------------------------------------------------
+English:
+Given two numbers entered via keyboard as data, 
+calculate their sum and report the result. 
+Declare the variables to be used: N1, N2, SUM, all of numerical type.
+
+Spanish: 
+Dados como datos (ingresados por teclado) dos números cualesquiera 
+calcular su suma e informar el resultado.
+Declarar las variables a utilizar N1, N2, SUMA de tipo numérico
+----------------------------------------------------------------------
+*/
+
+int n1,n2,sum;
+
+Console.WriteLine("Ingresar 2 numeros");
+n1 = int.Parse(Console.ReadLine());
+n2 = int.Parse(Console.ReadLine());
+
+sum = n1 + n2;
+Console.WriteLine("El resultado es:{0}",sum);
