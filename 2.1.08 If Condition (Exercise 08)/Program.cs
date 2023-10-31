c1963f7e5122dc4a0927b6650e5f92cb6a9f5391
JuanBeltrame <jbeltrame@frro.utn.edu.ar>
2023-10-31 13:12:07 -0300

    Update Program.cs

--- 2.1.08 If Condition (Exercise 08)/Program.cs
+++ 2.1.08 If Condition (Exercise 08)/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Practica 1 - If Conditional - Exercise 08
+----------------------------------------------------------------------
+English:
+
+
+Spanish: 
+
+----------------------------------------------------------------------
+*/
