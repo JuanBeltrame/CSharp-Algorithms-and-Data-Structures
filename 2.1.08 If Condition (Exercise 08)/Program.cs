5bceeb7a7b80bc216a8aaa433ca3392e31cfe8da
JuanBeltrame <jbeltrame@frro.utn.edu.ar>
2023-11-09 15:10:19 -0300

    Update

--- 2.1.08 If Condition (Exercise 08)/Program.cs
+++ 2.1.08 If Condition (Exercise 08)/Program.cs
@@ -2,9 +2,59 @@
 Practica 1 - If Conditional - Exercise 08
 ----------------------------------------------------------------------
 English:
+Three positive numbers are read. 
+Determine whether they are the lengths of the sides of a triangle. 
+Remember that in any triangle, each side is less than or equal to the sum of the other two sides and less than their difference (it is sufficient to show this for one side). 
+If so, report whether the triangle is:
 
+Equilateral (3 equal sides),
+Isosceles (2 equal sides), or
+Scalene (3 different sides)
 
 Spanish: 
+Se leen tres números positivos. 
+Determinar si son las longitudes de los lados de un triángulo: 
+Recordar que en todo triangulo cada lado es menor o igual que la suma de los
+otros dos y menor que su diferencia (basta mostrarlo para un lado). 
+En caso afirmativo, informar si el mismo es:
 
+equilátero (3 lados iguales), 
+isósceles (2 lados iguales) o
+escaleno (3 lados distintos).
 ----------------------------------------------------------------------
 */
+
+
+int numeroA = int.Parse(Console.ReadLine()!);
+int numeroB = int.Parse(Console.ReadLine()!);
+int numeroC = int.Parse(Console.ReadLine()!);
+
+
+if (EsTriangulo(numeroA, numeroB, numeroC))
+{
+    if (numeroA == numeroB & numeroB == numeroC)
+    {
+        Console.WriteLine("Es un triangulo EQUILATERO ");
+    }
+    else if (numeroA != numeroB && numeroB != numeroC)
+    {
+        Console.WriteLine("Es un triangulo ESCALENO");
+    }
+    else
+    {   
+        Console.WriteLine("Es un triangulo ISOSCELES");
+    }
+}
+else
+{
+    Console.WriteLine("No es un triangulo Valido");
+}
+
+static bool EsTriangulo(int a, int b, int c)
+{
+    if (a <= b + c && b <= a + c && c <= a + b && a < Math.Abs(b - c) && b< Math.Abs(a - c) && c < Math.Abs(a - b))
+        return true;
+    else
+        return false;
+}
+
---
+++ 2.1.09 If Condition (Exercise 09)/Program.cs
@@ -0,0 +1,14 @@
+﻿/*
+Practica 1 - If Conditional - Exercise 09
+----------------------------------------------------------------------
+English:
+
+
+Spanish: 
+Una distribuidora de libros vende a librerías y a particulares. Aplica bonificaciones por
+cantidad según el siguiente criterio:
+- Librerías: hasta 24 unidades, el 20%; más de 24 unidades, el 25%.
+- Particulares: menos de 6 unidades, nada; desde 6 hasta 18 unidades, el
+5%; y más de 18 unidades, el 10%.
+----------------------------------------------------------------------
+*/
