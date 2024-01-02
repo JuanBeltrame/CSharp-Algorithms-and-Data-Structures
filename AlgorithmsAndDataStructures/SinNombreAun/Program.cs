23c4339755ad4a8edb15e45ad9e9cd7f4fd92617
JuanBeltrame <jbeltrame@frro.utn.edu.ar>
2024-01-02 13:26:08 -0300

    Update

---
+++ AlgorithmsAndDataStructures/SinNombreAun/Program.cs
@@ -0,0 +1,55 @@
+﻿/* 
+Ejercicio I05 - Tirame un centro - UTN Avellaneda
+----------------------------------------------------------------------
+English:
+
+
+Spanish: 
+Un centro numérico es un número que separa una lista de números enteros
+(comenzando en 1) en dos grupos de números, cuyas sumas son iguales.
+El primer centro numérico es el 6, 
+el cual separa la lista (1 a 8) en los grupos: (1; 2; 3; 4; 5) y (7; 8) cuyas sumas son ambas iguales a 15. 
+El segundo centro numérico es el 35, el cual separa la lista (1 a 49) en los grupos: (1 a 34) y (36 a 49) cuyas sumas son ambas iguales a 595.
+Se pide elaborar una aplicación que calcule los centros numéricos entre 1 y el número que el usuario ingrese por consola.
+----------------------------------------------------------------------
+*/
+
+
+Console.Title = "Ejercicio I05 - Tirame un centro";
+
+Console.WriteLine("Ingrese un numero: ");
+
+int numero;
+
+while (int.TryParse(Console.ReadLine(), out numero))
+{
+    // Recorro desde el 1 hasta el numero que ingreso el usuario
+    for (int centro = 1; centro < numero; centro++)
+    {
+        // Sumo los numeros inferiores al centro
+        int sumaAntes = 0;
+        for (int i = 0; i < centro; i++)
+        {
+            sumaAntes += i;
+        }
+
+        // Sumo los numeros superiores al centro
+        int sumaDespues = 0;
+        for (int j = centro+1; j <= sumaAntes; j++)
+        {
+            if((sumaAntes == sumaDespues) || (sumaDespues > sumaAntes))
+            {
+                break;
+            }
+            sumaDespues += j;
+        }
+
+        // Mostrar en pantalla
+        if (sumaAntes == sumaDespues)
+        {
+            Console.WriteLine("Es un centro numerico: {0}", centro);
+        }
+    }
+}
+Console.WriteLine("No es un numero");
+Console.ReadKey();
