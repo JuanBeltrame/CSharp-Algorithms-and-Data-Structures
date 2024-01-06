e361660ea19e121ca6efffed85dca8c18181db14
Juan Beltrame <juan_beltrame@hotmail.com>
2024-01-06 16:33:53 -0300

    Update Program.cs

--- AlgorithmsAndDataStructures/9.1.02 Star Patterns Programs/Program.cs
+++ AlgorithmsAndDataStructures/9.1.02 Star Patterns Programs/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* 
+Ejercicio I09 - Dibujando un triángulo equilátero - UTN Avellaneda
+----------------------------------------------------------------------
+English:
+Write a program that prints by console a triangle like the following:
+
+        *
+       ***
+      *****
+     *******
+    *********
+
+The user will indicate the height of the triangle by entering a positive integer. 
+For the example above, the height entered was 5.
+
+Spanish: 
+Escribir un programa que imprima por consola un triángulo como el siguiente:
+
+        *
+       ***
+      *****
+     *******
+    *********
+
+El usuario indicará cuál será la altura del triángulo ingresando un número entero positivo. 
+Para el ejemplo anterior, la altura ingresada fue de 5.
+----------------------------------------------------------------------
+*/
+
+//----Programa Principal------
+
+Console.Title = "Ejercicio I09 - Dibujando un triángulo equilátero - UTN Avellaneda";
+Console.ForegroundColor = ConsoleColor.Yellow;
