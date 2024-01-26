9ad843213f593f0ac99f5592c31affaf7c4042ca
Juan Beltrame <juan_beltrame@hotmail.com>
2024-01-25 22:34:29 -0300

    u

--- AlgorithmsAndDataStructures/2.1.13 If Condition (Exercise 13)/Program.cs
+++ AlgorithmsAndDataStructures/2.1.13 If Condition (Exercise 13)/Program.cs
@@ -4,8 +4,8 @@ Practica 1 - If Conditional - Exercise 2.1.13
 English:
 
 Spanish: 
-Se dan dos valores cualesquiera enteros. Si el primero es mayor que el segundo, restarle
-al primero un 20%, pero si el segundo es el mayor restarle al segundo un 15%. Con estos
+Se dan dos valores cualesquiera enteros. 
+Si el primero es mayor que el segundo, restarle al primero un 20%, pero si el segundo es el mayor restarle al segundo un 15%. Con estos
 nuevos valores, si el primero quedo mayor a 100 y el segundo mayor a 150, se lo
 consideran valores correctos, de lo contrario es un intervalo de riesgo.
 ----------------------------------------------------------------------
@@ -17,3 +17,4 @@ Console.ForegroundColor = ConsoleColor.Yellow;
 
 int valor01 = default;
 int valor02 = default;
+
