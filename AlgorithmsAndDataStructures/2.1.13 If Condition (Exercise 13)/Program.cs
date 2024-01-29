3260fb4ddd5f713d61c150fcb50fd57db926b5dd
Juan Beltrame <juan_beltrame@hotmail.com>
2024-01-29 19:34:44 -0300

    Update

--- AlgorithmsAndDataStructures/2.1.13 If Condition (Exercise 13)/Program.cs
+++ AlgorithmsAndDataStructures/2.1.13 If Condition (Exercise 13)/Program.cs
@@ -2,12 +2,20 @@
 Practica 1 - If Conditional - Exercise 2.1.13
 ----------------------------------------------------------------------
 English:
+Any two integer values are given. 
+If the first is greater than the second, subtract 20% from the first, 
+but if the second is greater, subtract 15% from the second. 
+With these new values, if the first is greater than 100 and the second greater than 150, they are considered correct values.
+considered correct values, 
+otherwise it is a risk interval.
 
 Spanish: 
 Se dan dos valores cualesquiera enteros. 
-Si el primero es mayor que el segundo, restarle al primero un 20%, pero si el segundo es el mayor restarle al segundo un 15%. Con estos
-nuevos valores, si el primero quedo mayor a 100 y el segundo mayor a 150, se lo
-consideran valores correctos, de lo contrario es un intervalo de riesgo.
+Si el primero es mayor que el segundo, restarle al primero un 20%, 
+pero si el segundo es el mayor restarle al segundo un 15%. 
+Con estos nuevos valores, si el primero quedo mayor a 100 y el segundo mayor a 150, se lo
+consideran valores correctos, 
+de lo contrario es un intervalo de riesgo.
 ----------------------------------------------------------------------
 */
 
@@ -18,3 +26,28 @@ Console.ForegroundColor = ConsoleColor.Yellow;
 int valor01 = default;
 int valor02 = default;
 
+Console.Write("Ingresar el primer numero: ");
+int.TryParse(Console.ReadLine(), out valor01);
+Console.Write("Ingresar el primer numero: ");
+int.TryParse(Console.ReadLine(), out valor02);
+
+
+if (valor01 > valor02)
+{
+    decimal valorAdescontar = 0.2M * valor01;
+    valor01 = valor01 - (int)valorAdescontar;
+}
+else
+{
+    decimal valorADescontar = 0.15M * valor02;
+    valor02 = valor02 - (int)valorADescontar;
+}
+
+if (valor01 > 100 && valor02 > 150)
+{
+    Console.WriteLine("Los valores ingresados estan en el rango correcto");
+}
+else
+{
+    Console.WriteLine("Los valores ingresados pertenecen al rando de riesgo");
+}
---
+++ AlgorithmsAndDataStructures/3.1.1 For Loops (Exercise 01)/Program.cs
@@ -0,0 +1,37 @@
+﻿/* 
+Practica 1 - For loops - Exercise 3.1.1
+----------------------------------------------------------------------
+English:
+Calculate the salary of each of the 50 workers in a factory,
+given as data the hourly wage (it is the same for all workers) and the number of hours worked in the month by each worker. 
+and the number of hours worked in the month by each worker.
+
+Spanish: 
+Calcular el sueldo de cada uno de los 50 operarios de una fábrica,
+dados como datos la remuneración por hora (es la misma para todos los operarios) 
+y la cantidad de horas que trabajó en el mes cada operario.
+----------------------------------------------------------------------
+*/
+
+//--------Programa Principal----------
+Console.Title = "Practica 1 - For loops - Exercise 3.1.1";
+Console.ForegroundColor = ConsoleColor.Yellow;
+
+Console.Write("Ingresar la remuneracion por hora: ");
+if (decimal.TryParse(Console.ReadLine(), out decimal precioHora))
+{
+    
+    int cantidadOperarios = 3;
+    for (int i = 0; i < cantidadOperarios; i++)
+    {
+        Console.WriteLine("Ingresar la cantidad de horas trabajadas por el operario: ");
+        int horasTrabajadas = int.Parse(Console.ReadLine()!);
+        decimal salario = precioHora * horasTrabajadas;
+        Console.WriteLine($"El operario: {i + 1}, cobra un salario de: {salario}");
+    }
+
+}
+else
+{
+    Console.WriteLine("Solo se deben ingresar numeros");
+}
