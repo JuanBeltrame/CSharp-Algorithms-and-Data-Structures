c0584debe3e34aff79e11c6c693d7dd2e7ca8cb6
Juan Beltrame <juan_beltrame@hotmail.com>
2024-01-29 19:36:38 -0300

    Update

--- AlgorithmsAndDataStructures/3.1.2 For Loops (Exercise 02)/Program.cs
+++ AlgorithmsAndDataStructures/3.1.2 For Loops (Exercise 02)/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* 
+Practica 1 - For loops - Exercise 3.1.2
+----------------------------------------------------------------------
+English:
+Given 100 integers as input, 
+display each of them indicating whether it is
+'POSITIVE' or 'NEGATIVE', as appropriate.
+
+Spanish: 
+Dados como datos 100 números enteros, 
+mostrar cada uno de ellos indicando si es
+‘POSITIVO’ o ‘NEGATIVO’, según corresponda.
+----------------------------------------------------------------------
+*/
+
+//--------Programa Principal----------
+Console.Title = "Practica 1 - For loops - Exercise 3.1.2";
+Console.ForegroundColor = ConsoleColor.Yellow;
