b405eb1213d9eb7ee576a1025dfcd63e6fd07d19
Juan Beltrame <juan_beltrame@hotmail.com>
2024-01-29 23:13:06 -0300

    Update

--- AlgorithmsAndDataStructures/3.1.2 For Loops (Exercise 02)/Program.cs
+++ AlgorithmsAndDataStructures/3.1.2 For Loops (Exercise 02)/Program.cs
@@ -16,3 +16,22 @@ mostrar cada uno de ellos indicando si es
 //--------Programa Principal----------
 Console.Title = "Practica 1 - For loops - Exercise 3.1.2";
 Console.ForegroundColor = ConsoleColor.Yellow;
+
+int cantidad = 5;
+
+for (int i = 0; i < cantidad; i++)
+{
+    Console.Write("Ingresar un numero: ");
+    int.TryParse(Console.ReadLine(), out int valor);
+
+    if (valor > 0)
+    {
+        Console.WriteLine("El numeroo ingresado es positivo");
+    }
+    else if(valor < 0)
+    {
+        Console.WriteLine("El numero ingresado es negativo");
+    }
+    else
+        Console.WriteLine("El numero ingresado es cero");
+}
---
+++ AlgorithmsAndDataStructures/3.1.3 For Loops (Exercise 03)/Program.cs
@@ -0,0 +1,35 @@
+﻿/* 
+Practica 1 - For loops - Exercise 3.1.3
+----------------------------------------------------------------------
+English:
+Entering a sequence of 300 integers, 
+determine the number of positive numbers in it.
+
+Spanish: 
+Ingresando una sucesión de 300 números enteros, 
+determinar la cantidad de números positivos que hay en ella.
+----------------------------------------------------------------------
+*/
+
+//--------Programa Principal----------
+Console.Title = "Practica 1 - For loops - Exercise 3.1.3";
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.BackgroundColor = ConsoleColor.Black;
+
+int cantidad = 5;
+int contador = default;
+for (int i = 0; i < cantidad; i++)
+{
+    Console.Write("Ingresar un numero: ");
+    int.TryParse(Console.ReadLine(), out int numero);
+    if (numero > 0)
+    {
+        contador += 1;
+    }
+    else
+    {
+
+    }
+    
+}
+Console.WriteLine($"Se ingresaron {contador} numeros positivos");
