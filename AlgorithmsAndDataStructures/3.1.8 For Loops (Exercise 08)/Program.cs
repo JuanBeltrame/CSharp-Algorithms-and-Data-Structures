3bf85a35ad38ef9473397580c815b125840c704f
Juan Beltrame <juan_beltrame@hotmail.com>
2024-01-30 22:05:38 -0300

    Update

---
+++ AlgorithmsAndDataStructures/3.1.8 For Loops (Exercise 08)/Program.cs
@@ -0,0 +1,42 @@
+﻿/* 
+Practica 1 - For loops - Exercise 3.1.8
+----------------------------------------------------------------------
+English:
+Enter two natural numbers.
+Check if the first one is smaller than the second one.
+If so, display all the numbers between them in ascending sequence, including the extremes.
+sequence, including the extremes.
+
+Spanish: 
+Ingresar dos números naturales.
+Verificar si el primero es menor que el segundo.
+En caso afirmativo mostrar todos los números comprendidos entre ellos en secuencia
+ascendente, incluyendo los extremos.
+----------------------------------------------------------------------
+*/
+
+//--------Programa Principal----------
+
+using System.Diagnostics.Tracing;
+
+Console.Title = "Practica 1 - For loops - Exercise 3.1.8";
+Console.ForegroundColor = ConsoleColor.Yellow;
+
+int numero01 = default;
+int numero02 = default;
+
+Random rn = new();
+
+Console.Write("Ingresar el primer numero: ");
+numero01 = int.Parse(Console.ReadLine()!);
+Console.Write("Ingresar el segundo numero: ");
+numero02 = int.Parse(Console.ReadLine()!);
+
+if(numero01 < numero02)
+{
+    for (int i = numero01; i <= numero02; i++)
+    {
+        Console.Write(i + " - ");
+        
+    }
+}
---
+++ AlgorithmsAndDataStructures/3.1.9 For Loops (Exercise 09)/Program.cs
@@ -0,0 +1,21 @@
+﻿/* 
+Practica 1 - For loops - Exercise 3.1.9
+----------------------------------------------------------------------
+English:
+The amounts of 200 sales of a bookstore are given as data. We wish to know:
+- How many sales had amounts less than $ 10000.
+- What is the total amount of the sales whose amount was equal to or greater than $ 10000.
+
+Spanish: 
+Se dan como datos los importes de las 200 ventas de una librería. Se desea saber:
+• Cuántas ventas tuvieron importes menores que $ 10000.
+• Cuál es el monto total de las ventas cuyo importe fue igual o mayor que $ 10000.-
+----------------------------------------------------------------------
+*/
+
+//--------Programa Principal----------
+
+using System.Diagnostics.Tracing;
+
+Console.Title = "Practica 1 - For loops - Exercise 3.1.9";
+Console.ForegroundColor = ConsoleColor.Yellow;
