3e7b38f69e9f7a4f8e79e43685220de2b5e18762
Juan Beltrame <juan_beltrame@hotmail.com>
2024-01-31 22:38:18 -0300

    Update

--- AlgorithmsAndDataStructures/3.1.10 For Loops (Exercise 10)/Program.cs
+++ AlgorithmsAndDataStructures/3.1.10 For Loops (Exercise 10)/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* 
+Practica 1 - For loops - Exercise 3.1.10
+----------------------------------------------------------------------
+English:
+Obtain the sum of the N natural numbers after the number 300 inclusive.
+
+Spanish: 
+Obtener la suma de los N números naturales posteriores al número 300 inclusive.
+----------------------------------------------------------------------
+*/
+
+//--------Programa Principal----------
+
+using System.Diagnostics.Tracing;
+
+Console.Title = "Practica 1 - For loops - Exercise 3.1.10";
+Console.ForegroundColor = ConsoleColor.Yellow;
+
+Console.Write("Ingresar la cantidad de numeros que se desean sumar: ");
+int cantidad = int.Parse(Console.ReadLine()!);
+int acumulador = 0;
+
+for (int i = 300; i <= 300 + cantidad; i++)
+{
+    acumulador += i;
+}
+
+Console.WriteLine($"La suma obtenida es: {acumulador} ");
---
+++ AlgorithmsAndDataStructures/3.1.11.1 For Loops (Exercise 11)/Program.cs
@@ -0,0 +1,34 @@
+﻿/* 
+Practica 1 - For loops - Exercise 3.1.11
+----------------------------------------------------------------------
+English:
+Generate and report the first N multiples of any integer M.
+N and M are two numbers that are entered as data.
+
+Spanish: 
+Generar e informar los primeros N múltiplos de un número M entero cualquiera.
+N y M son dos números que se ingresan como dato.
+----------------------------------------------------------------------
+*/
+
+//--------Programa Principal----------
+
+using System.Diagnostics.Tracing;
+
+Console.Title = "Practica 1 - For loops - Exercise 3.1.10";
+Console.ForegroundColor = ConsoleColor.Yellow;
+
+Console.Write("Ingresar un numero (M): ");
+int M = int.Parse(Console.ReadLine()!);
+Console.Write("Ingresar la cantidad de multiplos (N) a obtener de (M): ");
+int N = int.Parse(Console.ReadLine()!);
+
+int resultado = 0;
+
+for (int i = 0; i <= N ; i++)
+{
+    resultado = i * M;
+    Console.WriteLine($"los multiplos de {M} son: {resultado}");
+}
+
+
