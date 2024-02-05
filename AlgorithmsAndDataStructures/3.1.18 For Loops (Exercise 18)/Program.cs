3830e990d9c791483fc0145a1f27d054fd6ae600
JuanBeltrame <jbeltrame@frro.utn.edu.ar>
2024-02-05 16:22:28 -0300

    UPdate

--- AlgorithmsAndDataStructures/3.1.18 For Loops (Exercise 18)/Program.cs
+++ AlgorithmsAndDataStructures/3.1.18 For Loops (Exercise 18)/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* 
+Practica 1 - For loops - Exercise 3.1.18
+----------------------------------------------------------------------
+English:
+
+
+Spanish: 
+Una escuela realiza un control sobre el estado físico de sus 304 alumnos. Dispone de
+los números de legajos y estatura (en cms.) de cada uno de ellos.
+Se requiere saber el promedio de estatura, así como los números de legajos de los
+alumnos de estatura inferior a 165 cms.
+----------------------------------------------------------------------
+*/
+
+//--------Programa Principal----------
+
+using Microsoft.Win32.SafeHandles;
+using System.Globalization;
+using System.Runtime.ExceptionServices;
+
+Console.Title = "Practica 1 - For loops - Exercise 3.1.18";
