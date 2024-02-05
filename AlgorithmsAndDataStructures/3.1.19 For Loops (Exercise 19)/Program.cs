c128f0b4fcd310d25a35f57a8d44b164a372e19c
Juan Beltrame <juan_beltrame@hotmail.com>
2024-02-05 19:56:49 -0300

    Update

---
+++ AlgorithmsAndDataStructures/3.1.19 For Loops (Exercise 19)/Program.cs
@@ -0,0 +1,20 @@
+﻿/* 
+Practica 1 - For loops - Exercise 3.1.19
+----------------------------------------------------------------------
+English:
+
+
+
+Spanish: 
+
+
+----------------------------------------------------------------------
+*/
+
+//--------Programa Principal----------
+
+using Microsoft.Win32.SafeHandles;
+using System.Globalization;
+using System.Runtime.ExceptionServices;
+
+Console.Title = "Practica 1 - For loops - Exercise 3.1.19";
---
+++ AlgorithmsAndDataStructures/3.1.20 For Loops (Exercise 20)/Program.cs
@@ -0,0 +1,40 @@
+﻿/* 
+Practica 1 - For loops - Exercise 3.1.20
+----------------------------------------------------------------------
+English:
+A committee has 55 students, 
+Each one of them has the grades of the 6 mid-term exams they have taken. 
+Obtain the average of the grades of each of the students.
+
+Spanish: 
+Una comisión tiene 55 alumnos, 
+de cada uno de los cuales se tienen las notas de los 6 parciales que han rendido. 
+Obtener el promedio de las notas de cada uno de los alumnos.
+----------------------------------------------------------------------
+*/
+
+//--------Programa Principal----------
+
+using Microsoft.Win32.SafeHandles;
+using System.Globalization;
+using System.Runtime.ExceptionServices;
+
+Console.Title = "Practica 1 - For loops - Exercise 3.1.20";
+Console.ForegroundColor = ConsoleColor.Yellow;
+
+int cantidadAlumnos = 55;
+
+for (int i = 0; i < cantidadAlumnos; i++)
+{
+    int cantidadDeParciales = 6;
+    int acumuladorNotas = 0;
+    for (int j = 0; j < cantidadDeParciales; i++)
+    {
+        int nota = default;
+        Random rn = new();
+        nota = rn.Next(1, 11);
+        acumuladorNotas = acumuladorNotas + nota;
+    }
+    Console.WriteLine($"Alumno: {i+1}, con un promedio de: {acumuladorNotas}");
+}
+
