f69affb57dda881307049b33650bc4726af98945
JuanBeltrame <jbeltrame@frro.utn.edu.ar>
2024-02-05 14:28:46 -0300

    Update

--- AlgorithmsAndDataStructures/3.1.15 For Loops (Exercise 15)/Program.cs
+++ AlgorithmsAndDataStructures/3.1.15 For Loops (Exercise 15)/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* 
+Practica 1 - For loops - Exercise 3.1.15
+----------------------------------------------------------------------
+English:
+
+
+Spanish: 
+Generar e informar los primeros 23 términos de la sucesión de Fibonacci. 
+Tener en cuenta que los dos primeros términos son iguales a uno y que los restantes se obtienen
+como la suma de los dos anteriores.
+----------------------------------------------------------------------
+*/
+
+//--------Programa Principal----------
+
+Console.Title = "Practica 1 - For loops - Exercise 3.1.15";
+Console.ForegroundColor = ConsoleColor.Yellow;
