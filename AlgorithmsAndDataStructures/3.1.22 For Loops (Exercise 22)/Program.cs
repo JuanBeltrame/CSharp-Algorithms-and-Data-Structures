a1e381001200750a847b49dfee7f02f3fb2d6593
JuanBeltrame <jbeltrame@frro.utn.edu.ar>
2024-02-06 16:46:39 -0300

    Update

--- AlgorithmsAndDataStructures/3.1.22 For Loops (Exercise 22)/Program.cs
+++ AlgorithmsAndDataStructures/3.1.22 For Loops (Exercise 22)/Program.cs
@@ -2,10 +2,10 @@
 Practica 1 - For loops - Exercise 3.1.22
 ----------------------------------------------------------------------
 English:
-
+You have a text of 190 characters. Determine how many times the syllable "pa" appears.
 
 Spanish: 
-
+Se cuenta con un texto de 190 caracteres. Determinar cuántas veces aparece la sílaba “pa”.
 ----------------------------------------------------------------------
 */
 
@@ -17,3 +17,4 @@ using System.Runtime.ExceptionServices;
 
 Console.Title = "Practica 1 - For loops - Exercise 3.1.22";
 Console.ForegroundColor = ConsoleColor.Yellow;
+
---
+++ AlgorithmsAndDataStructures/3.1.23 For Loops (Exercise 23)/Program.cs
@@ -0,0 +1,84 @@
+﻿/* 
+Practica 1 - For loops - Exercise 3.1.23
+----------------------------------------------------------------------
+English:
+A company is divided into 10 sections. 
+For each of them we have the following data:
+- Section no.
+- Number of employees working in it
+for each employee we have the following data:
+- Number of hours worked
+- Working shift ('M': morning; 'T': afternoon)
+
+You want to know:
+- The average number of hours worked in each section
+- The total number of hours worked in each shift in the company.
+
+Spanish: 
+Una empresa está dividida en 10 secciones. 
+Para cada una de ellas se tienen como datos:
+• Nro. de sección
+• Cantidad de empleados que trabajan en ella
+para cada empleado se tiene como dato:
+• Cantidad de horas trabajadas
+• Turno de trabajo (‘M’: mañana; ‘T’: tarde)
+
+Se desea saber:
+• El promedio de horas trabajadas en cada sección
+• La cantidad total de horas trabajadas en cada turno en la empresa
+----------------------------------------------------------------------
+*/
+
+//--------Programa Principal----------
+
+using Microsoft.Win32.SafeHandles;
+using System.Globalization;
+using System.Runtime.ExceptionServices;
+
+Console.Title = "Practica 1 - For loops - Exercise 3.1.23";
+Console.ForegroundColor = ConsoleColor.Yellow;
+
+int cantidadSecciones = 10;
+int cantidadEmpleados;
+Random rn = new();
+
+int acumuladorHorasTMGlobal = 0;
+int acumuladorHorasTTGlobal = 0;
+
+for (int i = 0; i < cantidadSecciones; i++)
+{
+    int promedio = 0;
+    int cantidadHorasTrabajadas;
+    int turno;
+    int acumuladorHoras = 0;
+    int acumuladorHorasTM = 0;
+    int acumuladorHorasTT = 0;
+
+    Console.WriteLine($"Numero de Seccio: {i+1}");
+    cantidadEmpleados = rn.Next(1, 21);
+    Console.WriteLine($"Actualmente trabajan {cantidadEmpleados}");
+    for (int j = 0; j < cantidadEmpleados; j++)
+    {
+        cantidadHorasTrabajadas = rn.Next(20, 41);
+        acumuladorHoras = acumuladorHoras + cantidadHorasTrabajadas;
+        Console.WriteLine($"El empleado: {j}, trabajo: {cantidadHorasTrabajadas} horas.");
+        turno = rn.Next(0, 2);
+        if ( turno == 0)
+        {
+            acumuladorHorasTM = acumuladorHorasTM + cantidadHorasTrabajadas;
+            acumuladorHorasTMGlobal += cantidadHorasTrabajadas;
+        }
+        else
+        {
+            acumuladorHorasTT = acumuladorHorasTT + cantidadHorasTrabajadas;
+            acumuladorHorasTTGlobal += cantidadHorasTrabajadas;
+        }
+    }
+
+    promedio = acumuladorHoras / cantidadEmpleados;
+    Console.WriteLine($"El promedio de horas trabajadas en la seccion {i+1}, es de: {promedio}");
+    Console.WriteLine($"En la seccion {i +1} se trabajaron: {acumuladorHorasTM} horas a la manaana y {acumuladorHorasTT} a la tarde");
+}
+
+Console.WriteLine($"En toda la empresa se trabajaron {acumuladorHorasTMGlobal} horas en el turno manana");
+Console.WriteLine($"En toda la empresa se trabajaron {acumuladorHorasTTGlobal} horas en el turno tarde");
