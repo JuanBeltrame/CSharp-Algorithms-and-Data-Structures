822b3600631762ab63b54deaf5c716ae4e5f4f3b
Juan Beltrame <juan_beltrame@hotmail.com>
2024-02-08 21:07:00 -0300

    Upodate

---
+++ AlgorithmsAndDataStructures/3.2.1.5 While Loops (Exercise 05)/Program.cs
@@ -0,0 +1,19 @@
+﻿/* 
+Practica 1 - While loops - Exercise 3.2.1.5
+----------------------------------------------------------------------
+English:
+OK
+
+Spanish: 
+Se cuenta con la información brindada por un conjunto de vendedores. Por cada uno de
+ellos se ingresa su código (un carácter distinto de *) y el importe total de sus ventas.
+Determinar el código del vendedor con mayor importe vendido y dicho importe.
+----------------------------------------------------------------------
+*/
+
+//--------Programa Principal----------
+
+using System.Security.Cryptography;
+
+Console.Title = "Practica 1 - While loops - Exercise 3.2.1.5";
+Console.ForegroundColor = ConsoleColor.Yellow;
