84eb849017baaa909e6acd271a3ee39a95a247a7
Juan Beltrame <juan_beltrame@hotmail.com>
2024-02-12 18:10:14 -0300

    Update

---
+++ AlgorithmsAndDataStructures/3.2.1.6 While Loops (Exercise 06)/Program.cs
@@ -0,0 +1,28 @@
+﻿/* 
+Practica 1 - While loops - Exercise 3.2.1.6
+----------------------------------------------------------------------
+English:
+A company has 50 salesmen working for it. 
+At the end of the month each of the salesmen reports their number and the amounts of each of the sales made. 
+The number of sales made by each of them is not known. 
+so a sales value equal to zero indicates that there are no more sales from that salesperson.
+You are asked to exhibit, 
+for each of the salesmen, the salesman's number and the amount of the highest sale made by that salesman.
+
+Spanish: 
+Una empresa tiene 50 viajantes que trabajan en ella. 
+A fin de mes cada uno de los viajantes informa su número y los importes de cada una de las ventas realizadas. 
+No se sabe la cantidad de ventas que realizó cada uno de ellos 
+por lo que un valor de venta igual a cero indica que no hay más ventas de ese vendedor.
+Se pide exhibir, 
+para cada uno de los viajantes, el Nro. del viajante y el importe de la mayor venta realizada por el mismo.
+----------------------------------------------------------------------
+*/
+
+//--------Programa Principal----------
+
+using System.Collections.Concurrent;
+using System.Security.Cryptography;
+
+Console.Title = "Practica 1 - While loops - Exercise 3.2.1.6";
+Console.ForegroundColor = ConsoleColor.Yellow;
