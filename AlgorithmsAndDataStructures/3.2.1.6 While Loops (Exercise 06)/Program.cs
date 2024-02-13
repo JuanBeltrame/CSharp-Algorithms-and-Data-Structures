1e95cfa9372268f77ad8478658793c9f26764fe2
Juan Beltrame <juan_beltrame@hotmail.com>
2024-02-13 17:05:32 -0300

    Update

--- AlgorithmsAndDataStructures/3.2.1.6 While Loops (Exercise 06)/Program.cs
+++ AlgorithmsAndDataStructures/3.2.1.6 While Loops (Exercise 06)/Program.cs
@@ -22,17 +22,51 @@ para cada uno de los viajantes, el Nro. del viajante y el importe de la mayor ve
 //--------Programa Principal----------
 
 using System.Collections.Concurrent;
+using System.Runtime.InteropServices;
 using System.Security.Cryptography;
 
 Console.Title = "Practica 1 - While loops - Exercise 3.2.1.6";
 Console.ForegroundColor = ConsoleColor.Yellow;
 
-int cantidadDeViajantes = 50;
-Console.WriteLine("--------------");
-Console.WriteLine($"Cantidad de Viajantes de la empresa: {cantidadDeViajantes}");
+int cantidadDeViajantes = 10;
+int importe;
+int contadorVentas = 0;
+int acumuladorDeVentas = 0;
+int ventaMaxima = 0;
+int pos = default;
+int contadorGlobal = 0;
+Random rn = new();
 
+Console.WriteLine("--------------");
+Console.WriteLine($"La Cantidad de Viajantes que posee la empresa es: {cantidadDeViajantes}");
+Console.WriteLine("--------------");
+Console.WriteLine();
 
 for (int i = 0; i < cantidadDeViajantes; i++)
 {
+    Console.WriteLine();
+    contadorVentas = 0;
+    acumuladorDeVentas = 0;
+    ventaMaxima = 0;
+    Console.WriteLine($"Numero de Viajante: {i + 1}");
 
+    importe = rn.Next(0, 26);
+    Console.WriteLine($"Ingresar importe de la venta [0 para salir]: {importe}");
+    while (importe != 0)
+    {
+        contadorVentas += 1;
+        importe = rn.Next(0, 26);
+        acumuladorDeVentas = acumuladorDeVentas + importe;
+        if (importe > ventaMaxima)
+        {
+            ventaMaxima = importe;
+            pos = i;
+        }
+
+        Console.WriteLine($"Ingresar importe de la venta [0 para salir]: {importe}");
+    }
+
+    Console.WriteLine("-----------");
+    Console.WriteLine($"El vendedor {pos}, realizo {contadorVentas} ventas, y recaudo ${acumuladorDeVentas} en total. su venta maxima fue de: {ventaMaxima}");
+    Console.WriteLine("-----------");
 }
---
+++ AlgorithmsAndDataStructures/3.2.1.7 While Loops (Exercise 07)/Program.cs
@@ -0,0 +1,49 @@
+﻿/* 
+Practica 1 - While loops - Exercise 3.2.1.7
+----------------------------------------------------------------------
+English:
+At the end of each day, 
+The salespeople in a store report their sales to the owner to calculate the commission they will receive. 
+There are 8 salespeople, coded 'A' to 'H', and it is not known how many sales each one made.
+The data is sorted and grouped by salesperson. 
+For each salesman each of the amounts of his sales are entered. 
+To indicate the end of each one of them, a sales value equal to 0 is entered. 
+It is requested to show for each of the salesmen:
+his code and the commission he will charge, which is 2.5% of the sum of his sales.
+
+Spanish: 
+Al finalizar cada día, 
+Los vendedores de un comercio rinden al dueño sus ventas para calcular la comisión que cobrarán. 
+Los vendedores son 8, codificados de la ‘A’ a la ‘H’, y no se sabe cuántas ventas realizó cada uno.
+Los datos vienen ordenados y agrupados por vendedor. 
+Por cada vendedor se ingresan cada uno de los importes de sus ventas. 
+Para indicar el fin de cada uno de ellos se ingresa un valor de venta igual a 0. 
+Se solicita mostrar para cada uno de los vendedores:
+su código y la comisión que cobrará, que es el 2,5 % de la suma de sus ventas.
+----------------------------------------------------------------------
+*/
+
+//--------Programa Principal----------
+
+using System.Runtime.InteropServices;
+
+Console.Title = "Practica 1 - While loops - Exercise 3.2.1.7";
+Console.ForegroundColor = ConsoleColor.Yellow;
+
+char cantidadDeVendedores = 'H';
+int importe = default;
+Random rn = new();
+
+for (char i = 'A'; i < cantidadDeVendedores; i++)
+{
+    importe = rn.Next(0, 51);
+    Console.WriteLine($"Importe del vendedor {i}: {importe}");
+    while (importe != 0)
+    {
+        importe = rn.Next(0, 51);
+        Console.WriteLine($"Importe del vendedor {i}: {importe}");
+    }
+
+
+    Console.WriteLine($"El importe del vendedor {i}, es de {importe}");
+}
