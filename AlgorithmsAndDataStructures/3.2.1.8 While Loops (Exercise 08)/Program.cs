35fc09677eb9fd5bee3c9089b2117f0829138432
Juan Beltrame <juan_beltrame@hotmail.com>
2024-02-14 04:11:23 -0300

    Update

---
+++ AlgorithmsAndDataStructures/3.2.1.8 While Loops (Exercise 08)/Program.cs
@@ -0,0 +1,25 @@
+﻿/* 
+Practica 1 - While loops - Exercise 3.2.1.8
+----------------------------------------------------------------------
+English:
+
+
+Spanish: 
+Se desea saber el total de ventas de cada uno de los vendedores de una empresa. A tal
+fin se tienen como datos: el código de vendedor y el importe de cada una de las ventas;
+un vendedor puede haber realizado más de una venta. No se sabe la cantidad de
+vendedores que tiene la empresa ni la cantidad de ventas hechas por cada vendedor (un
+código de vendedor igual a cero es fin de datos). ESTOS DATOS ESTAN ORDENADOS
+POR CODIGO DE VENDEDOR. Exhibir cada código de vendedor y su total
+correspondiente y al final, el código de vendedor con mayor importe vendido y dicho
+importe.
+----------------------------------------------------------------------
+*/
+
+//--------Programa Principal----------
+
+using System.Runtime.InteropServices;
+
+Console.Title = "Practica 1 - While loops - Exercise 3.2.1.8";
+Console.ForegroundColor = ConsoleColor.Yellow;
+
---
+++ AlgorithmsAndDataStructures/3.3.1.1 do while loop/Program.cs
@@ -0,0 +1,31 @@
+﻿/* 
+Practica 1 - While loops - Exercise 3.3.1.1
+----------------------------------------------------------------------
+English:
+
+
+Spanish: 
+Determinar cuántos caracteres tiene un conjunto de elementos, sabiendo que el último
+carácter perteneciente al conjunto es una “S”, y que ésta letra aparece sólo una vez.
+----------------------------------------------------------------------
+*/
+
+//--------Programa Principal----------
+
+using System.Runtime.InteropServices;
+
+Console.Title = "Practica 1 - While loops - Exercise 3.3.1.1";
+Console.ForegroundColor = ConsoleColor.Yellow;
+
+char elemento = default ;
+int contElementos = 0;
+
+do
+{
+    Console.WriteLine("Ingresar un elemento: ");
+    elemento = char.Parse(Console.ReadLine()!);
+    contElementos += 1;
+
+} while (char.ToUpper(elemento) != 'S');
+
+Console.WriteLine($"El conjunto de caracteres ingresados tuvo: {contElementos} elementos");
