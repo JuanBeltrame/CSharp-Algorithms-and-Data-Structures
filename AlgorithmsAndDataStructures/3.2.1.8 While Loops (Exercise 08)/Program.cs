567c99ae1073b47532c238a5a6d692f2abaec595
Juan Beltrame <61248615+JuanBeltrame@users.noreply.github.com>
2024-03-04 11:07:41 -0300

    Update Program.cs

--- AlgorithmsAndDataStructures/3.2.1.8 While Loops (Exercise 08)/Program.cs
+++ AlgorithmsAndDataStructures/3.2.1.8 While Loops (Exercise 08)/Program.cs
@@ -1,18 +1,27 @@
-﻿/* 
-Practica 1 - While loops - Exercise 3.2.1.8
+/*
+Practica 1 - While Loops - Pre-Conditional - Exercise 01/08
 ----------------------------------------------------------------------
-English:
+English: 
+We want to know the total sales for each of the sellers in a company. 
+To this end, we have the following data: the seller's code and the amount of each sale.
 
+A seller may have made more than one sale. 
+We don't know the number of sellers the company has or the number of sales made by each seller 
+(a seller code equal to zero marks the end of data). 
+THIS DATA IS SORTED BY SELLER CODE. 
+Display each seller's code and their corresponding total, 
+and finally, the seller code with the highest total sales and that total amount."
 
 Spanish: 
-Se desea saber el total de ventas de cada uno de los vendedores de una empresa. A tal
-fin se tienen como datos: el código de vendedor y el importe de cada una de las ventas;
-un vendedor puede haber realizado más de una venta. No se sabe la cantidad de
-vendedores que tiene la empresa ni la cantidad de ventas hechas por cada vendedor (un
-código de vendedor igual a cero es fin de datos). ESTOS DATOS ESTAN ORDENADOS
-POR CODIGO DE VENDEDOR. Exhibir cada código de vendedor y su total
-correspondiente y al final, el código de vendedor con mayor importe vendido y dicho
-importe.
+Se desea saber el total de ventas de cada uno de los vendedores de una empresa. 
+A tal fin se tienen como datos: el código de vendedor y el importe de cada una de las ventas;
+
+Un vendedor puede haber realizado más de una venta. 
+No se sabe la cantidad de vendedores que tiene la empresa ni la cantidad de ventas hechas por cada vendedor 
+(un código de vendedor igual a cero es fin de datos). 
+ESTOS DATOS ESTAN ORDENADOS POR CODIGO DE VENDEDOR. 
+Exhibir cada código de vendedor y su total correspondiente 
+y al final, el código de vendedor con mayor importe vendido y dicho importe.
 ----------------------------------------------------------------------
 */
 
