162149a0825ec2793575a031a1091c9179a500f0
Juan Beltrame <juan_beltrame@hotmail.com>
2024-03-07 07:24:54 -0300

    Update Program.cs

--- AlgorithmsAndDataStructures/3.2.1.8 While Loops (Exercise 08)/Program.cs
+++ AlgorithmsAndDataStructures/3.2.1.8 While Loops (Exercise 08)/Program.cs
@@ -27,7 +27,40 @@ y al final, el código de vendedor con mayor importe vendido y dicho importe.
 
 //--------Programa Principal----------
 
+using System.Runtime.InteropServices;
 
+Console.Title = "Practica 1 - While loops - Exercise 3.2.1.8";
+Console.ForegroundColor = ConsoleColor.Yellow;
+
+
+int codVendedor = default;
+int mayorImporteGlobal = 0;
+int vendedorMax = default;
+
+Console.Write($"Ingresar codigo de Vendedor (0 para finalizar): ");
+int codigoVendedor = int.Parse(Console.ReadLine()!);
+
+while (codigoVendedor != 0)
+{
+    int acumuladorTotalxVendedor = 0;
+    int vendedorAnterior = codigoVendedor;
+    while (codigoVendedor == vendedorAnterior)
+    {
+        Console.Write($"Ingresar Importe de la venta realizada por el vendedor {codigoVendedor}: ");
+        int importe = int.Parse(Console.ReadLine()!);
+        acumuladorTotalxVendedor = acumuladorTotalxVendedor + importe;
+        if (acumuladorTotalxVendedor > mayorImporteGlobal)
+        {
+            vendedorMax = codigoVendedor;
+            mayorImporteGlobal = acumuladorTotalxVendedor;
+        }
+
+        Console.Write($"Ingresar codigo de Vendedor (0 para finalizar): ");
+        codigoVendedor = int.Parse(Console.ReadLine()!);
+    }
+    Console.WriteLine($"El importe total del vendedor: {vendedorAnterior}, es de: {acumuladorTotalxVendedor}");
+}
+Console.WriteLine($"El vendedor con el mayor importe vendido es: {vendedorMax}, con un monto de: {mayorImporteGlobal}");
 
 
 
